ea8b46aae3b35d19e9c0d11822ac33671accdefe
Priteshkag <122962687+Priteshkag@users.noreply.github.com>
2024-02-15 18:12:41 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,6 +3,74 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+let
+stack
+= [];
+   
+let
+mapping
+= {
+       
+")":
+"(",
+       
+"}":
+"{",
+       
+"]":
+"["
+    };
+   
+const
+stringLength
+=
+s.length;
+   
+for (let
+i
+=
+0;
+i
+<
+stringLength;
+i++)
+ {
+       
+let
+char
+=
+s[i];
+       
+if (mapping[char])
+ {
+           
+let
+stackTop
+=
+stack.pop();
+           
+if (mapping[char]
+!==
+stackTop) {
+               
+return
+false;
+            }
+        }
+else {
+           
+stack.push(char);
+        }
+    }
+
+
+   
+return
+stack.length
+===
+0;
+
+
     
 };
 
